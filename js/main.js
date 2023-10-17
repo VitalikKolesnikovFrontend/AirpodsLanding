44c5e2a3475d421821853863f29971ecb1378b79
Vitalik Kolesnikov <VitaliKolesnikov22@gmail.com>
2023-10-17 18:46:24 +0300

    added js. finished work on the project

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const chooseColor = document.querySelectorAll('.choose-color__btn');
+const contentItem = document.querySelectorAll('.content-item');
+
+chooseColor.forEach(function(element) {
+    element.addEventListener('click', open)
+})
+
+function open(event) {
+    const target = event.currentTarget;
+    const button = target.dataset.button;
+    const contentActive = document.querySelectorAll(`.${button}`);
+    
+    chooseColor.forEach(function(item) {
+        item.classList.remove('choose-color__btn--active')
+    })
+
+    target.classList.add('choose-color__btn--active')
+
+    contentItem.forEach((item) => {
+        item.classList.remove('content-item__active')
+    })
+
+    contentActive.forEach((item) => {
+        item.classList.add('content-item__active')
+    })
+
+
+}  
